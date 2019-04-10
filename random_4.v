4f3fb5d024b3ac610e56edf28fa65adbafb90638
Md-Khalid-1129 <48331153+Md-Khalid-1129@users.noreply.github.com>
2019-04-11 01:09:11 +0530

    Update random_4.v

--- random_4.v
+++ random_4.v
@@ -12,44 +12,27 @@ reg [3:0] reset_value = 13;
 reg [3:0] lfsr;
 reg [2:0] count;
 
-// pragma translate_off 
-//integer f;
-//initial begin
-//f = $fopen("output.txt","w");
-//end
-// pragma translate_on
-
 always @ (posedge clock or posedge reset)
 begin
-if (reset) begin
-lfsr <= reset_value; 
-count <= 3'h7;
-rnd <= 0;
-end
-else begin
-lfsr <= lfsr_next;
-count <= count + 1;
+  if (reset) begin
+    lfsr <= reset_value; 
+    count <= 3'h7;
+    rnd <= 0;
+  end
+  else begin
+    lfsr <= lfsr_next;
+    rnd <= lfsr;
+    count <= count + 1;
 // a new random value is ready
-if (count == 3'd2) begin 
-count <= 0;
-rnd <= lfsr; //assign the lfsr number to output after 10 shifts
-// pragma translate_off 
-//$fwrite(f,"%0d\n",rnd);
-// pragma translate_on 
-end
-end
+    if (count == 3'd7) begin 
+      count <= 0;
+//rnd <= lfsr; //assign the lfsr number to output after 10 shifts 
+    end
+   end
 end
 
 // X4+x3
 assign feedback = lfsr[3] ^ lfsr[2]; 
 assign lfsr_next = {lfsr[2:0], feedback};
 
-// pragma translate_off
-//always @ (*) begin 
-//if (rnd == reset_value) begin
-//$fclose(f); 
-//$finish();
-//end
-//end
-// pragma translate_on
 endmodule
