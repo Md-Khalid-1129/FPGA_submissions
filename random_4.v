6a21dbd38d234716f49d478f9a5dea98212de97e
Deep Diwani <ddiwani3008@gmail.com>
2019-04-22 02:05:01 +0530

    Update random_4.v

--- random_4.v
+++ random_4.v
@@ -1,33 +1,34 @@
-module rndm_gen (input clock, input reset, output reg [7:0] rnd);
+module rndm_gen (input wire clk, input reset, output reg [7:0] rnd);
 
 wire feedback;
 wire [7:0] lfsr_next;
 
 //An LFSR cannot have an all 0 state, thus reset to non-zero value
-reg [26:0] delay;
+reg [26:0] delay = 0;
 reg [7:0] reset_value = 13;
-reg [7:0] lfsr;
+reg [7:0] lfsr = 13;
 reg [3:0] count;
 
-always @ (posedge clock)
+always @ (posedge clk)
 begin
-	delay <= delay+1;
-	if(delay==27'b000000011110100001001000000)
+	delay = delay+1;
+	if(delay==27'b101111101011110000100000000)
 	begin
+		delay = 27'b0;
 		if (reset)
 		begin
-			lfsr <= reset_value; 
-			count <= 4'hF;
-			rnd <= 13;
+			lfsr = reset_value; 
+			count = 0;
+			rnd = 13;
 		end
 		else
 		begin
-			lfsr <= lfsr_next;
-			rnd <= lfsr;
-			count <= count + 1;
+			lfsr = lfsr_next;
+			rnd = lfsr_next;
+			count = count + 1;
 			if (count == 4'd15)
 			begin 
-				count <= 0;
+				count = 0;
 			end
 		end
 	end
