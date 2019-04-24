2a2cf91ffef24175f117e2309332900682aa1756
Deep Diwani <ddiwani3008@gmail.com>
2019-04-24 17:40:00 +0530

    Update random_4.v

--- random_4.v
+++ random_4.v
@@ -1,19 +1,21 @@
-module rndm_gen (input wire clk, input [7:0] reset_value, input wire reset, output reg [7:0] rnd);
+module rndm_gen (input wire clk, input [7:0] reset_value, input wire reset, output reg [9:0] rnd, output reg [1:0] sig);
 
 wire feedback;
 wire [7:0] lfsr_next;
 
-//An LFSR cannot have an all 0 state, thus reset to non-zero value
-reg [26:0] delay = 0;
+reg [26:0] time_value = 0;
 reg [7:0] lfsr = 13;
-reg [3:0] count;
+reg [9:0] count;
+reg [9:0] half = 511;
+reg [9:0] one_fourth = 255;
+reg [9:0] three_fourth = 767;
 
 always @ (posedge clk)
 begin
-	delay = delay+1;
-	if (delay==27'b101111101011110000100000000)
+	time_value = time_value+1;
+	if (time_value==27'b101111101011110000100000000)
 	begin
-		delay = 27'b0;
+		time_value = 27'b0;
 		if (reset)
 		begin
 			lfsr = reset_value; 
@@ -24,6 +26,24 @@ begin
 		begin
 			lfsr = lfsr_next;
 			rnd = lfsr_next;
+			
+			if(rnd <= one_fourth)
+		    begin
+			    sig = 0;
+		    end
+		    else if(rnd <= half)
+		    begin
+			    sig = 1;
+		    end
+		    else if (rnd <= three_fourth)
+		    begin
+			    sig = 2;
+		    end
+		    else
+		    begin
+			    sig = 3;
+		    end
+			
 			count = count + 1;
 			if (count == 4'd15)
 			begin 
@@ -33,9 +53,7 @@ begin
 	end
 end
 
-
-assign feedback = lfsr[7] ^lfsr[5] ^lfsr[4] ^ lfsr[3]; 
-assign lfsr_next = {lfsr[6:0], feedback};
-
+assign feedback = lfsr[9] ^lfsr[6]; 
+assign lfsr_next = {lfsr[8:0], feedback};
 
 endmodule
